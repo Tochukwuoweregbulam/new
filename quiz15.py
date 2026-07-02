0787e60666d38d9dd9937a59ac63cec1590d2121
Tochukwuoweregbulam <oweregbulamt@gmail.com>
2026-07-02 11:20:34 +0100

    add new features

--- quiz15.py
+++ quiz15.py
@@ -2,26 +2,40 @@ from abc import ABC, abstractmethod
 
 class Users(ABC):
 
+   def __init__(self):
+       self.name = input("Please enter your name: ")
+
    @abstractmethod
    def login (self):
       pass
    @abstractmethod
    def logout(self):
       pass
+class Result:
+    def Get_User(self):
+     user = input("Welcome, Are a teacher or student?")
+     if user == "student":
+         return Students()
+
+     elif user == "teacher":
+         return Teachers()
+
+
 
 class Students(Users):
     def login (self):
-        print("Welcome Students")
+        print("Welcome", self.name)
 
     def logout(self):
-        print("Goodbye Students")
+        print("Goodbye", self.name)
 
 class Teachers(Users):
     def login(self):
-        print("Welcome Teachers")
+        print("Welcome ")
     def logout(self):
         print("Goodbye Teachers")
 
-
-teachers = Teachers()
-teachers.login()
+result = Result()
+person = result.Get_User()
+person.login()
+person.logout()
